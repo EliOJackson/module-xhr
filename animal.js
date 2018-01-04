5707581d85ece8da7485ac30f5f615b8944f3515
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-01-04 16:28:03 -0600

    set up animal.js

--- animal.js
+++ animal.js
@@ -1,14 +1,29 @@
 let carnivores = [];
+let herbivores = [];
 
-loadCarnivores = function(callbackToInvoke) {
+let loadCarnivores = function(callback) {
   const loader = new XMLHttpRequest();
-
+  
+  loader.open("GET", "data/carnivores.json");
+  loader.send();
+    
   loader.addEventListener("load", function() {
-    // Set the value of the private array
     carnivores = JSON.parse(this.responseText);
-
-    // Now exeute the callback function (`callbackToInvoke`) so that the caller knows that the process is complete. Make sure to pass the carnivore array as an argument.
+    carnivores = carnivores.carnviores; 
+    callback(carnivores);
   });
 };
 
-module.exports = { loadCarnivores };
+let loadHerbivores = function(callback) {
+    const loader = new XMLHttpRequest();
+    
+    loader.open("GET", "data/herbivores.json");
+    loader.send();
+      
+    loader.addEventListener("load", function() {
+      herbivores = JSON.parse(this.responseText);
+      herbivores = herbivores.herbivores; 
+      callback(herbivores);
+    });
+  };
+module.exports = { loadCarnivores, loadHerbivores };
