3b57accbc9bcde12accc0d81dbfe3857f58f51e8
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-01-16 22:22:09 -0600

    trying to print to the dom but getting object:object

--- javascripts/animal.js
+++ javascripts/animal.js
@@ -3,24 +3,26 @@ let $ = require('jquery');
 let herbivores = [];
 let carnivores = [];
 
- module.exports.loadHerbivores = () => {
+ module.exports.loadHerbivores = (callBackToInvoke) => {
   $.ajax({
     url: "../data/herbivores.json" 
   })
   .done( (herbivoreData) => {
     herbivores = herbivoreData.herbivores;
+    callBackToInvoke(herbivores);
     console.log("herbivores plz", herbivores);
   }).fail( (error) => {
     console.log("fail boat", error.statusText);
   });
 };
 
-module.exports.loadCarnivores = () => {
+module.exports.loadCarnivores = (callBackToInvoke) => {
   $.ajax({
     url: "../data/carnivores.json" 
   })
   .done( (carnivoreData) => {
     carnivores = carnivoreData.carnivores;
+    callBackToInvoke(carnivores);
     console.log("carnivores plz", carnivores);
   }).fail( (error) => {
     console.log("fail boat", error.statusText);
